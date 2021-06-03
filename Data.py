d0130c76267ab6d652a707c35ee656cb21efee3c
rohitkuk <rohit.kukreja01@gmail.com>
2021-06-03 11:53:26 +0530

    Anime Dataset Extraction Corrected

--- Data.py
+++ Data.py
@@ -9,10 +9,16 @@ def kaggle_dataset(args):
 
     if args.dataset == "pokemon":
         path = 'lantian773030/pokemonclassification'
+        out_path = "dataset"
     
     elif args.dataset == "anime":
         path = "arnaud58/ffhq-flickr-faces-align-crop-and-segment"
-    
+        try:
+            os.mkdir('Images')
+        except:
+            pass
+
+        out_path = "dataset/Images"
     file_name = path.split('/')[-1] + ".zip"
 
     while not args.kaggle_user:
@@ -41,9 +47,10 @@ def kaggle_dataset(args):
     api.dataset_download_files(path, quiet=False)
 
     print("#######Extracting Dataset #####")
+
     with ZipFile(file=file_name) as zip_file:
         for file in tqdm(iterable=zip_file.namelist(), total=len(zip_file.namelist())):
-            zip_file.extract(member=file, path='dataset')
+            zip_file.extract(member=file, path=out_path)
 
     print("#######Deleting Zip File #####")
     os.remove(file_name)
